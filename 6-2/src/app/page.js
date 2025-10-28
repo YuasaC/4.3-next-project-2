98d9c05fb68cc69e1a2d0552ea770b9024fdffe0
yuasa <coumo628@gmail.com>
2025-10-29 00:20:15 +0900

    コミット

--- 6-2/src/app/page.js
+++ 6-2/src/app/page.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useState, useEffect } from "react";
+import styles from './page.module.css'
 
 
 export default function UserRegister() {
@@ -40,37 +41,47 @@ export default function UserRegister() {
     setSubmit(true);
   }
   return (
-    <div>
+    <div className={styles.container}>
       <form onSubmit={handleSubmit}>
-        <label>
+        <div className={styles.groupContainer}>
           <input
             type="text"
             placeholder="ユーザー名"
             value={name}
             onChange={(event) => setName(event.target.value)} />
-          {!isNameValid && <p>ユーザー名は英文字のみ、4文字以上です。</p>}
-        </label>
-        <label>
+          <p className={styles.msg}>
+            {!isNameValid && "ユーザー名は英文字のみ、4文字以上です。"}
+          </p>
+        </div>
+        <div className={styles.groupContainer}>
           <input
             type="text"
             placeholder="メールアドレス"
             value={email}
             onChange={(event) => setEmail(event.target.value)} />
-          {!isEmailValid && <p>正しい形式のメールアドレスを入力してください。</p>}
-        </label>
-        <label>
+          <p className={styles.msg}>
+            {!isEmailValid && "正しい形式のメールアドレスを入力してください。"}
+          </p>
+        </div>
+        <div className={styles.groupContainer}>
           <input
             type="password"
             placeholder="パスワード"
             value={password}
             onChange={(event) => setPassword(event.target.value)} />
-          {!isPasswordValid && <p>パスワードは8文字以上です。</p>}
-        </label>
-        <button type="submit" disabled={!name || !email || !password}>
-          登録
-        </button>
+          <p className={styles.msg}>
+            {!isPasswordValid && "パスワードは8文字以上です。"}
+          </p>
+        </div>
+        <div className={styles.registration}>
+          <button type="submit" disabled={!name || !email || !password}>
+            登録
+          </button>
+        </div>
       </form>
-      {submit && <p>登録完了</p>}
-    </div>
+      <div>
+        {submit && <p>登録完了</p>}
+      </div>
+    </div> //form-container
   );
 }
