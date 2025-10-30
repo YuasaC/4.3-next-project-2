05dc0ba8b8453b07ac9aabe8b0619dfff336668f
yuasa <coumo628@gmail.com>
2025-10-30 22:06:04 +0900

    コミット

--- 1-2/src/app/page.js
+++ 1-2/src/app/page.js
@@ -3,30 +3,50 @@
 import React, { useState, useEffect } from "react";
 
 export default function CountdownTimer() {
-  const [time, setTime] = useState(0); // 経過時間を管理する状態
+  const [time, setTime] = useState(0); // 残り秒数
   const [isRunning, setIsRunning] = useState(false); // タイマーの動作を管理する状態
-  const [inputMinutes, setInputMinutes] = useState(0); //フォームから受け取った分
-  const [inputSeconds, setInputSeconds] = useState(0); //フォームから受け取った秒
+  const [inputMinutes, setInputMinutes] = useState(""); //フォームから受け取った分 form欄なのでstring扱い
+  const [inputSeconds, setInputSeconds] = useState(""); //フォームから受け取った秒
+  const [message, setMessage] = useState(""); //カウントダウン0になった時のメッセージ表示
 
   //タイマー動作の設計
   useEffect(() => {
-    if (isRunning) {
+    if (isRunning && time > 0) {
       const interval = setInterval(() => {
         setTime((prevTime) => prevTime - 1);
       }, 1000);
       return () => clearInterval(interval); //クリーンアップ処理
     }
-  }, [isRunning]);
+  }, [isRunning, time]);//isRunning→スタート・ストップした時にinterval更新
+  // time→残り時間が0になったら停止処理
+
+  useEffect(() => {
+    if (time === 0 && isRunning) {
+      setIsRunning(false);
+      setMessage("カウントダウン終了✨");
+    }
+  }, [time])
+
+  //残り時間の表示を 分:秒に整形する
+  const formatTime = (t) => {
+    const minute = Math.floor(t / 60);
+    const second = t % 60;
+    return `${minute.toString().padStart(2, "0")}:${second.toString().padStart(2, "0")}`;
+  }
 
   //タイマースタート(実行)
-  const start = () => setIsRunning(true);
+  const start = () => {
+    const totalSeconds = Number(inputMinutes) * 60 + Number(inputSeconds); //分×60＋秒で合計秒数に変換
+    if (totalSeconds > 0) {
+      setTime(totalSeconds); //time更新
+      setIsRunning(true); //trueにすることでカウントダウン開始
+    } setInputMinutes("");
+    setInputSeconds("");
+  }
+
 
   //タイマーストップ(一時停止)
   const stop = () => setIsRunning(false);
-
-  //カウントダウン終了時
-  const finish = () => setTime(0);
-
 
   return (
     <div>
@@ -34,8 +54,11 @@ export default function CountdownTimer() {
       </input>
       <input type="number" value={inputSeconds} placeholder="秒" onChange={(event) => setInputSeconds(event.target.value)}></input>
       <button onClick={start}>スタート</button>
-      <button onClick={isRunning ? stop : start}>{isRunning ? "ストップ" : "再開"}</button>
-      <p onChange={finish}>Hey</p>
+      <button onClick={isRunning ? stop : start}>{isRunning ? "一時停止" : "再開"}</button>
+      <p>{formatTime(time)}</p>
+      <p>
+        {message}
+      </p>
     </div>
   )
 }
